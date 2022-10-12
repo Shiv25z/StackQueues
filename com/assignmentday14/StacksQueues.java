de9ae9c81d28ee7de7ab333feb53adf2cdfdfd53
Shiv25z <shivamzagade25899@gmail.com>
2022-10-12 14:49:05 +0530

    Welcome to Stack & Queues Problem!

---
+++ com/assignmentday14/StacksQueues.java
@@ -0,0 +1,8 @@
+package com.assignmentday14;
+
+public class StacksQueues {
+    public static void main(String[] args) {
+        System.out.println("Welcome to StackQueue Problem");
+        
+    }
+}
